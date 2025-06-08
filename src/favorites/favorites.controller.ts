4e4cac8bab70b53d9c08a8fd40dc65275d089091
andrewk0704 <andrew.kozhenov@mail.ru>
2025-06-08 10:40:57 +0400

    feat: update Favorites

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Controller, Get, HttpCode, ParseUUIDPipe, Param, Post, Delete } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 
 @Controller('favs')
@@ -11,9 +11,40 @@ export class FavoritesController {
     return this.favoritesService.getFavorites();
   }
 
-  // @Delete('artist/:id')
-  // @HttpCode(204)
-  // deleteArtistFavorites(@Param('id', ParseUUIDPipe) id: string) {
-  //   this.favoritesService.deleteArtistFavorites(id);
-  // }
+  @Post('album/:id')
+  @HttpCode(201)
+  createAlbumByIdFavorites(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favoritesService.createAlbumByIdFavorites(id);
+  }
+
+  @Post('track/:id')
+  @HttpCode(201)
+  createTrackByIdFavorites(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favoritesService.createTrackByIdFavorites(id);
+  }
+
+  @Post('artist/:id')
+  @HttpCode(201)
+  createArtistByIdFavorites(@Param('id', ParseUUIDPipe) id: string) {
+    return this.favoritesService.createArtistByIdFavorites(id);
+  }
+
+  @Delete('track/:id')
+  @HttpCode(204)
+  deleteTrackByIdFavorites(@Param('id', ParseUUIDPipe) id: string) {
+    this.favoritesService.deleteTrackByIdFavorites(id);
+  }
+
+  @Delete('album/:id')
+  @HttpCode(204)
+  deleteAlbumByIdFavorites(@Param('id', ParseUUIDPipe) id: string) {
+    this.favoritesService.deleteAlbumByIdFavorites(id);
+  }
+
+  @Delete('artist/:id')
+  @HttpCode(204)
+  deleteArtistByIdFavorites(@Param('id', ParseUUIDPipe) id: string) {
+    this.favoritesService.deleteArtistByIdFavorites(id);
+  }
+
 }
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { 
+  Injectable,
+  UnprocessableEntityException,
+  NotFoundException
+ } from '@nestjs/common';
 
 import { Artist, artistDb } from '../artist/artist.service';
 import { Album, albumDb } from '../album/album.service';
@@ -11,9 +15,7 @@ export interface FavoritesResponse {
   tracks: Track[];
 }
 
-// const artists=[];
-// const albums=[];
-// const tracks=[];
+
 
 export const favoritesDb = { artists: [], albums: [], tracks: [] };
 
@@ -27,22 +29,123 @@ export const db = {
 
 @Injectable()
 export class FavoritesService {
+  
   getFavorites() {
-    return db.favoritesDb;
+    const artists=[];
+    const albums=[];
+    const tracks=[];
+
+    db.favoritesDb.tracks.forEach((id) => {
+      const track = db.trackDb.find((track) => track.id === id);
+      if (track) {
+        tracks.push(track);
+      }
+    });
+
+    db.favoritesDb.albums.forEach((id) => {
+      const album = db.albumDb.find((album) => album.id === id);
+
+      if (album) {
+        albums.push(album);
+      }
+    });
+
+    db.favoritesDb.artists.forEach((id) => {
+      const artist = db.artistDb.find((artist) => artist.id === id);
+
+      if (artist) {
+        artists.push(artist);
+      }
+    });
+
+    // if(!tracks && !albums && !artists){
+    //   return undefined
+
+    // }
+
+    return {
+      tracks,
+      albums,
+      artists,
+    };
   }
 
-  // deleteArtistFavorites(id: string) {
-  //   const artist = db.favoritesDb.artists.find((item) => item.id === id);
+  createAlbumByIdFavorites(id: string) {
+    const index = db.albumDb.findIndex((album) => album.id === id);
 
-  //   if (!artist) {
-  //     throw new NotFoundException('Not found');
-  //   } else {
-  //     const artistIndex = db.favoritesDb.artists.findIndex(
-  //       (item) => item.id === id,
-  //     );
-  //     if (artistIndex === -1) return false;
-  //     db.favoritesDb.artists.splice(artistIndex, 1);
-  //     return true;
-  //   }
-  // }
+    if (index === -1) {
+      throw new UnprocessableEntityException();
+    }
+
+    db.favoritesDb.albums.push(id);
+
+    return index;
+  }
+
+  createArtistByIdFavorites(id: string) {
+    const index = db.artistDb.findIndex((artist) => artist.id === id);
+
+    if (index === -1) {
+      throw new UnprocessableEntityException();
+    }
+
+    db.favoritesDb.artists.push(id);
+
+    return index;
+  }
+
+  createTrackByIdFavorites(id: string) {
+    const index = db.trackDb.findIndex((track) => track.id === id);
+
+    if (index === -1) {
+      throw new UnprocessableEntityException();
+    }
+
+    db.favoritesDb.tracks.push(id);
+
+    return index;
+  }
+
+  deleteTrackByIdFavorites(id: string) {
+    const index = db.favoritesDb.tracks.findIndex((item) => item === id);
+
+    if (index === -1) {
+      throw new NotFoundException('Not found');
+    }
+
+    db.favoritesDb.tracks = db.favoritesDb.tracks.filter(
+      (track) => track !== id,
+    );
+
+    return;
+  }
+
+  deleteAlbumByIdFavorites(id: string) {
+    const index = db.favoritesDb.albums.findIndex((item) => item === id);
+
+    if (index === -1) {
+      throw new NotFoundException('Not found');
+    }
+
+    db.favoritesDb.albums = db.favoritesDb.albums.filter(
+      (album) => album !== id,
+    );
+
+    return;
+  }
+
+  deleteArtistByIdFavorites(id: string) {
+    const index = db.favoritesDb.artists.findIndex((item) => item === id);
+
+    if (index === -1) {
+      throw new NotFoundException('Not found');
+    }
+
+    db.favoritesDb.artists = db.favoritesDb.artists.filter(
+      (artist) => artist !== id,
+    );
+
+    return;
+  }
+
 }
